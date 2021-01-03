76593b5b421ef1c134e101739e5d6732eb7123f6
Damir Mihaljinec <damir.mihaljinec@gmail.com>
2021-01-03 15:36:43 +0100

    Added screenshots Removed logging for image loading

--- app/src/main/java/com/swapcard/apps/moviebrowser/android/ui/BindingAdapter.kt
+++ app/src/main/java/com/swapcard/apps/moviebrowser/android/ui/BindingAdapter.kt
@@ -6,13 +6,11 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target
 import com.swapcard.apps.moviebrowser.android.model.Movie
-import timber.log.Timber
 
 @BindingAdapter("imageUrl")
 fun setImageUrl(imageView: ImageView, movie: Movie?) {
     movie?.let {
         if (movie.posterUrl.isNotEmpty()) {
-            Timber.d("Loading image: ${movie.posterUrl}")
             Glide.with(imageView)
                     .load(Uri.parse(movie.posterUrl))
                     .override(Target.SIZE_ORIGINAL)
